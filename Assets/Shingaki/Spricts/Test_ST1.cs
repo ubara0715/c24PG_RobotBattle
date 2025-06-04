37e6582dd4a8a246dae8b6f365f3622deb9d054d
c24012 <c24012@std.it-college.ac.jp>
2025-06-04 12:31:42 +0900

    Test_ST1エラー修正

--- Assets/Shingaki/Spricts/Test_ST1.cs
+++ Assets/Shingaki/Spricts/Test_ST1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test_ST : MonoBehaviour
+public class Test_ST1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
