9171986a47892f8993343c894697457ca4fcc143
徠夢 友利 <c24016@std.it-college.ac.jp>
2025-06-05 11:41:55 +0900

    tagの名前を変更

--- Assets/Tomori/Scripts/WeaponsAtackScript.cs
+++ Assets/Tomori/Scripts/WeaponsAtackScript.cs
@@ -7,7 +7,7 @@ public class WeaponsAtackScript : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         //Bulletなどの攻撃する球を消すための処理（必要に応じて別スクリプトに移動してほしい）
-        if(other.gameObject.tag == "Ballier" || other.gameObject.tag == "Player")
+        if(other.gameObject.tag == "Barrier" || other.gameObject.tag == "Player")
         {
             this.gameObject.SetActive(false);
         }
