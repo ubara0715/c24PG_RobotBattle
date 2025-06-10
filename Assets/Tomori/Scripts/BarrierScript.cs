884166cf4fdf97b2abc680a7b454ae34fe93e879
徠夢 友利 <c24016@std.it-college.ac.jp>
2025-06-10 10:43:58 +0900

    こちら側からタグを使ってダメージを計算せず 別スクリプトから引数でダメージを食らう関数を呼べるようにする

--- Assets/Tomori/Scripts/BarrierScript.cs
+++ Assets/Tomori/Scripts/BarrierScript.cs
@@ -7,15 +7,16 @@ public class BarrierScript : MonoBehaviour
     bool isBarrier = false;
     [SerializeField] GameObject barrier;
 
-    [SerializeField] float barrierHP = 100;//仮のバリアHP
+    [SerializeField] int barrierHP = 150;//仮のバリアHP
+    [SerializeField] float energyPool = 10000;//仮のエネルギープール量
     public float playerHP = 500;//仮のプレイヤーHP
 
     //下記3つのダメージは別の別の人が作ったダメージに応じて変更
-    [SerializeField] float bulletAtack = 200;//仮の実弾兵器のダメージ
-    [SerializeField] float energyAtack = 300;//仮のエネルギー兵器のダメージ
-    [SerializeField] float missileAtack = 400;//仮のミサイル兵器のダメージ
-    [SerializeField] float contactAtack = 300;//仮の接触攻撃のダメージ
-    [SerializeField] float contactBonus = 100;//接触攻撃のボーナスポイント（仕様書に書かれていたため）
+    /*[SerializeField] int bulletAtack = 200;//仮の実弾兵器のダメージ
+    [SerializeField] int energyAtack = 300;//仮のエネルギー兵器のダメージ
+    [SerializeField] int missileAtack = 400;//仮のミサイル兵器のダメージ
+    [SerializeField] int contactAtack = 300;//仮の接触攻撃のダメージ*/
+    [SerializeField] int contactBonus = 100;//接触攻撃のボーナスポイント（仕様書に書かれていたため）
 
     [SerializeField] Transform playerTR;
     [SerializeField] float sizeMultiplier = 1.2f;
@@ -23,10 +24,18 @@ public class BarrierScript : MonoBehaviour
     GameObject currentBarrier;
 
     [SerializeField,Header("プレイヤーの位置")] Vector3 detectionCenter;  // 検出の中心点（プレイヤーの位置)）
-    [SerializeField,Header("変出範囲の半径")] float detectionRadius;
+    [SerializeField,Header("検出範囲の半径")] float detectionRadius;
     [SerializeField,Header("球のレイヤー")] LayerMask bulletLayer;
 
     Collider[] bullets;
+
+    public enum AttackType
+    {
+        Bullet,
+        Energy,
+        Missile,
+        Contact
+    }
 
     void Start()
     {
@@ -49,15 +58,25 @@ public class BarrierScript : MonoBehaviour
     //バリアをtrueにする
     void SetBarrier()
     {
-        if(bullets.Length > 0)
+        float diameter = currentBarrier.transform.localScale.x;
+        float cost = diameter * barrierHP;
+
+        if (bullets.Length > 0)
         {
-            isBarrier = true;
-            currentBarrier.SetActive(true);
+            if (!isBarrier && energyPool >= cost)
+            {
+                energyPool -= cost;
+                isBarrier = true;
+                currentBarrier.SetActive(true);
+            }
         }
         else
         {
-            isBarrier = false;
-            currentBarrier.SetActive(false);
+            if (isBarrier)
+            {
+                isBarrier = false;
+                currentBarrier.SetActive(false);
+            }
         }
     }
 
@@ -80,11 +99,64 @@ public class BarrierScript : MonoBehaviour
 
         currentBarrier = Instantiate(barrier, playerTR.position, Quaternion.identity, playerTR);
         currentBarrier.transform.localScale = Vector3.one * maxSize * sizeMultiplier;
+
+        detectionRadius = maxSize * sizeMultiplier * 1.1f;//バリアの大きさに合わせて検出範囲の半径も大きく
     }
 
 
+    public void ReceiveDamage(AttackType attackType, int damageAmount)
+    {
+        switch (attackType)
+        {
+            case AttackType.Bullet:
+                if (isBarrier)
+                {
+                    if (barrierHP <= damageAmount)
+                        playerHP += barrierHP - damageAmount;
+                    // else → バリアで完全防御
+                }
+                else
+                {
+                    playerHP -= damageAmount;
+                }
+                break;
+
+            case AttackType.Energy:
+                if (!isBarrier)
+                {
+                    playerHP -= damageAmount;
+                }
+                break;
+
+            case AttackType.Missile:
+                if (isBarrier)
+                {
+                    if (barrierHP <= damageAmount)
+                        playerHP += barrierHP - damageAmount;
+                }
+                else
+                {
+                    playerHP -= damageAmount;
+                }
+                break;
+
+            case AttackType.Contact:
+                int totalContactDamage = damageAmount + contactBonus;
+                if (isBarrier)
+                {
+                    if (barrierHP <= totalContactDamage)
+                        playerHP += barrierHP - totalContactDamage;
+                }
+                else
+                {
+                    playerHP -= totalContactDamage;
+                }
+                break;
+        }
+    }
+
     //ダメージを計算するためにtriggerを使う（playerのisTriggerをtrue）
-    private void OnTriggerEnter(Collider other)
+    /*private void OnTriggerEnter(Collider other)
     {
         //実弾兵器から発射される球のタグ
         if (other.gameObject.tag == "Bullet")
@@ -158,6 +230,6 @@ public class BarrierScript : MonoBehaviour
                 playerHP = playerHP - missileAtack;
             }
         }
-    }
+    }*/
 }
 
