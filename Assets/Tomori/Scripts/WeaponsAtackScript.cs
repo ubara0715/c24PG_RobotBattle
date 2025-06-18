f0b1f3d9ba61e7b0fc01dc5f214a32ca7d77f5d2
徠夢 友利 <c24016@std.it-college.ac.jp>
2025-06-18 09:57:39 +0900

    当たり判定をトリガーからコリジョンに変更

--- Assets/Tomori/Scripts/WeaponsAtackScript.cs
+++ Assets/Tomori/Scripts/WeaponsAtackScript.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class WeaponsAtackScript : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
         //Bulletなどの攻撃する球を消すための処理（必要に応じて別スクリプトに移動してほしい）
-        if(other.gameObject.tag == "Barrier" || other.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Barrier" || collision.gameObject.tag == "Player")
         {
             gameObject.SetActive(false);
         }
