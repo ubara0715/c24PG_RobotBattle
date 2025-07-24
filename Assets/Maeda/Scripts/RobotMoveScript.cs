7ed63bccca0069ed9bd5429e6051d837ab7cea7c
祐貴 前田 <c24024@std.it-college.ac.jp>
2025-07-24 10:21:19 +0900

    自分のフォルダをコピー

--- Assets/Maeda/Scripts/RobotMoveScript.cs
+++ Assets/Maeda/Scripts/RobotMoveScript.cs
@@ -23,29 +23,28 @@ public class RobotMoveScript : MonoBehaviour
     float rotateForce = 1.0f;
     [SerializeField, Header("最高速度")]
     float maxSpeed = 1.0f;
-
+    
     //回転制御bool
     bool isRotate = false;
 
     void Start()
     {
         robotRB = GetComponent<Rigidbody>();
-
+        
         SetMass();
 
         //テスト用
         StartCoroutine(MoveTarget(sphere));
-        //StartCoroutine(RotateRobot(sphere));
     }
 
     void Update()
     {
         //テスト用
-        if (Input.GetKeyDown(KeyCode.Space)) MoveUp();
+        if (Input.GetKey(KeyCode.Space)) MoveUp();
     }
 
-    //機体の質量を設定
-    void SetMass()
+    //機体の質量を設定(機体の重量が変わるたびにCoreScriptから呼ぶ)
+    public void SetMass()
     {
         //CoreScriptの重量変数をロボットの質量とする
         robotRB.mass = coreScript.weight;
@@ -53,29 +52,51 @@ public class RobotMoveScript : MonoBehaviour
 
     public void MoveUp()//ジャンプ
     {
-        if (energyScript.UseEnergy(1.0f))
+        robotRB.AddForce(transform.up * jumpForce, ForceMode.Force);
+
+        if (robotRB.velocity.y > maxSpeed)
         {
-            robotRB.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+            robotRB.velocity = new Vector3(
+                robotRB.velocity.x,
+                maxSpeed,
+                robotRB.velocity.z
+                );
         }
+
     }
 
     //ターゲットへの移動
     public IEnumerator MoveTarget(GameObject targetOBJ)
     {
         //条件は後々変更
-        while (Mathf.Abs(targetOBJ.transform.position.x - transform.position.x) > 1
-            || Mathf.Abs(targetOBJ.transform.position.z - transform.position.z) > 1)
+        while (true)
         {
-            if (energyScript.UseEnergy(0.01f))
+            if (transform.position.y - targetOBJ.transform.position.y <= -1
+                && energyScript.UseEnergy(0.1f)) 
             {
-                Vector3 direction = targetOBJ.transform.position - transform.position;
+                MoveUp();
+            }
 
+            Vector3 direction = targetOBJ.transform.position - transform.position;
+
+            //ターゲットに近づいたら終了(仮条件)
+            if (Mathf.Abs(direction.x) < (transform.localScale.x + targetOBJ.transform.localScale.x) / 2
+                && Mathf.Abs(direction.y) < (transform.localScale.y + targetOBJ.transform.localScale.y) / 2
+                && Mathf.Abs(direction.z) < (transform.localScale.z + targetOBJ.transform.localScale.z) / 2)
+                break;
+
+            direction.y = 0;//xz平面で移動する
+
+            if (Mathf.Abs(direction.x) > (transform.localScale.x + targetOBJ.transform.localScale.x) / 2
+                || Mathf.Abs(direction.z) > (transform.localScale.z + targetOBJ.transform.localScale.z) / 2
+                && energyScript.UseEnergy(0.01f))
+            {
                 //自分から相手の方向にAddForce
                 robotRB.AddForce(direction.normalized * moveForce, ForceMode.Force);
                 
                 //速度チェック
                 CheckVelocity();
-                
+
                 //回転中でないとき
                 if (!isRotate)
                 {
@@ -83,7 +104,11 @@ public class RobotMoveScript : MonoBehaviour
 
                     StartCoroutine(RotateRobot(targetOBJ));
                 }
-            }  
+            }
+            else
+            {
+                robotRB.velocity = new Vector3(0, robotRB.velocity.y, 0);
+            }
 
             yield return null;
         }
@@ -111,18 +136,21 @@ public class RobotMoveScript : MonoBehaviour
     //ロボットの回転
     IEnumerator RotateRobot(GameObject targetOBJ)
     {
-        Vector3 direction = targetOBJ.transform.position - transform.position;
-        
-        while (Vector3.Angle(transform.forward, direction) > 10)
+        while (true) 
         {
+            Vector3 direction = targetOBJ.transform.position - transform.position;
+
+            direction.y = 0;//xz平面で回転
+
             if (energyScript.UseEnergy(0.01f))
             {
-                direction = targetOBJ.transform.position - transform.position;
-
                 float angle = Vector3.SignedAngle(transform.forward, direction, transform.up);
                 
+                //ほぼ正面を向いたら終了
+                if (Mathf.Abs(angle) < 5) break;
+
                 //角度が小さい方に回転
-                robotRB.AddTorque(direction * -Mathf.Sign(angle) * rotateForce, ForceMode.Force);   
+                robotRB.AddTorque(transform.up * Mathf.Sign(angle) * rotateForce, ForceMode.Force);
             }
 
             yield return null;
